b4680e4c753db82356006fdc09b58e3b8b316aaa
Elam Day <elam.day.friedland@gmail.com>
2021-05-28 13:36:38 -0700

    fixed reading 1 less column than exists

--- main.cpp
+++ main.cpp
@@ -41,44 +41,46 @@ void serialDilution(Plate * plate, int r, int c, int n, double dilution){
 Plate read_csv(std::string filename){
     std::fstream fin;
     fin.open(filename, std::ios::in);
+    if(!fin.is_open()) throw std::runtime_error("Could not open file");
     std::string line, word, temp;
     std::vector<std::vector<std::pair<std::string, std::pair<double, double>>>> platevals;
-    int nrows = 1;
-    int ncols = 0;
     while (fin >> temp){
-        std::vector<std::string> compounds = {};
-        std::vector<double> concs,vols = {};
-        std::vector<std::pair<std::string, std::pair<double,double>>> thisrow;
-        for (int i=0; i <3; i++){
-            getline(fin, line);
-            std::stringstream s(line);
-            getline(s, word, ','); // skip first column (has labels)
-            switch (i){
-                case 0:
-                    while (getline(s, word, ',')) {
+            std::vector<std::string> compounds = {};
+            std::vector<double> concs,vols = {};
+            std::vector<std::pair<std::string, std::pair<double,double>>> thisrow;
+            for (int i=0; i <3; i++){
+                getline(fin, line);
+                std::stringstream s(line);
+                getline(s, word, ','); // skip first column (has labels)
+                switch (i){
+                    case 0:
+                        while (getline(s, word, ',')) {
+                            compounds.push_back(word);
+                        }
+                        getline(s, word);
                         compounds.push_back(word);
-                    }
-                    break;
-                case 1:
-                    while (getline(s, word, ',')) {
-                        concs.push_back(stod(word));
-                    }
-                    break;
-                case 2:
-                    while (getline(s, word, ',')) {
-                        vols.push_back(stod(word));
-                    }
-                    break;
-            }
+                        break;
+                    case 1:
+                        while (getline(s, word, ',')) {
+                            concs.push_back(stod(word));
+                        }
+                        break;
+                    case 2:
+                        while (getline(s, word, ',')) {
+                            vols.push_back(stod(word));
+                        }
+                        break;
+                }
 
-        }
-        for(int i=0; i < compounds.size(); i++)
-            thisrow.push_back(std::make_pair(compounds[i], std::make_pair(concs[i], vols[i])));
-        platevals.push_back(thisrow);
-        ncols = thisrow.size();
-        nrows++;
+            }
+            for(int i=0; i < compounds.size(); i++)
+                thisrow.push_back(std::make_pair(compounds[i], std::make_pair(concs[i], vols[i])));
+            platevals.push_back(thisrow);
+
     }
-    std::cout << "nrows:" << nrows << " ncols:" << ncols << std::endl;
+    int nrows = platevals.size();
+    int ncols = platevals[0].size();
+    std::cout << "nrows:" << platevals.size() << " ncols:" << platevals[0].size() << std::endl;
     size_t lastindex = filename.find_last_of(".");
     std::string platename = filename.substr(0, lastindex); // strip off .csv or whatever extension
     Plate finalplate = Plate(nrows, ncols, 20, platename);
@@ -102,10 +104,11 @@ int main(){
     /* std::cout << pp.changeWellContents(10,15, 10, .1, "abc") << std::endl; */
     /* std::cout << pp.getWells()[0][0].toString() << std::endl; */
 
-    pp.save();
     Plate b = read_csv(pp.getName() + ".csv");
+    /* std::cout << b.toString() << std::endl; */
     auto col = {b};
     std::cout << "read plate name" << b.getName() << std::endl;
+    std::cout << b.toString() << std::endl;
     auto a = find(col, "abc", .1);
 
 }
